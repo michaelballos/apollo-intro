aaae3ece2dee137ae7f53611e3f13195ad490937
michaelballos <ballos.michael@gmail.com>
2022-07-13 14:14:05 -0500

    Add query result wrapper

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <GlobalStyles />
+      <Pages />
+    </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
