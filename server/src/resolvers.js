c17c15e56dab4264ae487e348d8f4b9bfab71724
michaelballos <ballos.michael@gmail.com>
2022-07-14 13:42:06 -0500

    Add card page

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -6,11 +6,26 @@ const resolvers  = {
      tracksForHome: (_, __, {dataSources}) => {
        return dataSources.trackAPI.getTracksForHome();
      },
+     /**
+      * Get single track by id
+      */
+     track: (_, {id}, {dataSources}) => {
+       return dataSources.trackAPI.getTrack(id);
+     }
    },
    Track: {
+     /**
+      * Get author of a track
+      */
      author: ({authorId}, _, {dataSources}) => {
         return dataSources.trackAPI.getAuthor(authorId);
-     }
+     },
+     /**
+      * Get modules of a track
+      */
+      modules: ({id}, _, {dataSources}) => {
+        return dataSources.trackAPI.getTrackModules(id);
+      }
    }
 }
 
--- server/src/schema.js
+++ server/src/schema.js
@@ -4,22 +4,32 @@ const typeDefs = gql`
   type Query {
     "Get tracks array for homepage grid"
     tracksForHome: [Track!]!
+    track(id: ID!): Track  
   }
   
   "A track is a group of Modules that teaches about a specific topic"
   type Track {
-    id: ID!    
-    title: String  
-    author: Author!
-    thumbnail: String
-    length: Int
-    modulesCount: Int  
+    id:            ID!    
+    title:         String  
+    author:        Author!
+    thumbnail:     String
+    length:        Int
+    modulesCount:  Int  
+    description:   String
+    numberOfViews: Int  
+    modules:       [Module!]!  
+  }
+  
+  type Module {
+    id:     ID!   
+    title:  String!  
+    length: Int  
   }
   
   "Author of a complete track"
   type Author {
-    id: ID!
-    name: String
+    id:    ID!
+    name:  String
     photo: String
   }
 `;
