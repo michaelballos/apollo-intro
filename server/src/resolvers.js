bb367de3bbd92c6b37bd4dd4b8326fd10c2339e2
michaelballos <ballos.michael@gmail.com>
2022-07-14 14:16:55 -0500

    Define view increment mutation schema and resolver

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -13,9 +13,30 @@ const resolvers  = {
        return dataSources.trackAPI.getTrack(id);
      }
    },
-   Track: {
+   Mutation: {
+     incrementTrackViews: async (_, {id}, {dataSources}) => {
+       try {
+         const track = dataSources.trackAPI.incrementTrackViews(id);
+         return {
+           code: 200,
+           success: true,
+           message: `Successfully incremented number of views for track ${id}`,
+           track
+         };
+       } catch (error) {
+         const response = error.extensions.response;
+         return {
+           code: response.status,
+           success: false,
+           message: response.body,
+           track: null
+         };
+       }
+     }
+   },
+   Track : {
      /**
-      * Get author of a track
+      *  Get author of a track
       */
      author: ({authorId}, _, {dataSources}) => {
         return dataSources.trackAPI.getAuthor(authorId);
